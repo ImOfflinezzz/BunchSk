6feec492777eb219a6e06193b230ba19d6c7af72
ImKotEz <imkotez@gmail.com>
2016-11-22 16:39:51 +0700

    main class

---
+++ src/Start.java
@@ -0,0 +1,11 @@
+import com.wh1lec0d3r_.bunchsk.core.server.CoreServer;
+
+public class Start {
+
+    static CoreServer coreServer;
+
+    public static void main(String[] args) {
+
+        coreServer = new CoreServer();
+    }
+}
