315001b282e1af472c4a9c80222f3ce589cda77a
ImKotEz <imkotez@gmail.com>
2016-11-23 17:13:30 +0700

    client

--- src/com/wh1lec0d3r_/bunchsk/core/client/CoreClient.java
+++ src/com/wh1lec0d3r_/bunchsk/core/client/CoreClient.java
@@ -1,9 +1,33 @@
 package com.wh1lec0d3r_.bunchsk.core.client;
 
-public class CoreClient {
+import com.wh1lec0d3r_.bunchsk.core.client.config.ConfigData;
 
+import java.io.IOException;
+import java.net.Socket;
 
-    public CoreClient(String host, int port) {
+public class CoreClient extends Thread {
+
+    private ConfigData configData;
+    private Socket socket;
+
+    public CoreClient(ConfigData configData) {
+        System.out.println("Loading...");
+        this.configData = configData;
+        this.openSocket(configData.host, configData.port);
+    }
+
+    private void openSocket(String host, int port) {
+
+        try {
+            this.socket = new Socket(host, port);
+            System.out.println("socket opened");
+        } catch (IOException e) {
+            System.out.println("error on opening socket");
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
 
     }
 }
