456b0f4433b2b82aae184f438b7b277e9bc1d8a2
BuildTools <unconfigured@null.spigotmc.org>
2016-11-27 23:12:02 +0700

    some features added reading data added password

--- src/com/wh1lec0d3r_/bunchsk/core/server/ClientHandler.java
+++ src/com/wh1lec0d3r_/bunchsk/core/server/ClientHandler.java
@@ -1,9 +1,11 @@
 package com.wh1lec0d3r_.bunchsk.core.server;
 
+import com.wh1lec0d3r_.bunchsk.core.api.utils.Hash;
 import com.wh1lec0d3r_.bunchsk.core.server.packet.PacketManager;
 import com.wh1lec0d3r_.bunchsk.core.server.packet.YPacket;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -12,16 +14,45 @@ public class ClientHandler extends Thread {
     private Socket socket;
     private CoreServer coreServer;
 
+    private String name;
+
     public ClientHandler(Socket socket, CoreServer coreServer) {
         System.out.println("Creating client");
         this.socket = socket;
         this.coreServer = coreServer;
         System.out.println("Created client");
         System.out.println("Starting read data");
-        this.start();
+        //this.start();
+        this.readData();
+    }
+
+    private void readData() {
+        try {
+            DataInputStream dataInputStream = new DataInputStream(this.getSocket().getInputStream());
+            String name = dataInputStream.readUTF();
+            String password = dataInputStream.readUTF();
+            int hashId = dataInputStream.readInt();
+
+            this.name = name;
+            if(Hash.validate(Hash.getEnumById(hashId), password, this.getCoreServer().getConfigData().password))
+                this.start();
+            else {
+                System.out.println("logout");
+                this.getCoreServer().removeClient(this);
+                this.getSocket().close();
+            }
+
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+
     }
 
     public void run() {
+        System.out.println("succesefully");
         while (!this.getSocket().isClosed()) {
             try {
                 DataInputStream dataInputStream = new DataInputStream(this.getSocket().getInputStream());
@@ -49,4 +80,7 @@ public class ClientHandler extends Thread {
         return socket;
     }
 
+    public CoreServer getCoreServer() {
+        return coreServer;
+    }
 }
