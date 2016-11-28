2c4b79b201bbef9c3278ed1268a6e00ea1fdee11
BuildTools <unconfigured@null.spigotmc.org>
2016-11-28 18:05:06 +0700

    instance

--- src/Start.java
+++ src/Start.java
@@ -2,9 +2,11 @@ import com.wh1lec0d3r_.bunchsk.core.server.CoreServer;
 
 public class Start {
 
+    //idk
     static CoreServer coreServer;
 
     public static void main(String[] args) {
         coreServer = new CoreServer(args);
     }
+
 }
