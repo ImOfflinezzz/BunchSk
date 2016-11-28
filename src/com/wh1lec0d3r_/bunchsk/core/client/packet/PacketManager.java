0d050f894d90adfc5868fc4829d8323d5ca048b1
BuildTools <unconfigured@null.spigotmc.org>
2016-11-28 19:26:00 +0700

    packets for client

---
+++ src/com/wh1lec0d3r_/bunchsk/core/client/packet/PacketManager.java
@@ -0,0 +1,27 @@
+package com.wh1lec0d3r_.bunchsk.core.client.packet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PacketManager {
+
+    public static Map<Short, Class<? extends YPacket>> packets = new HashMap<>();
+
+    static
+    {
+        //todo add some packets
+    }
+
+    public static YPacket getPacket(short id)
+    {
+        try
+        {
+            return packets.get(id).newInstance();
+        }
+        catch (InstantiationException | IllegalAccessException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
