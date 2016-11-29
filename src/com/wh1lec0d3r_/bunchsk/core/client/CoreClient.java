021f5874e2848d2e2ef698245ad9d1e3aa8b732c
ImKotEz <imkotez@gmail.com>
2016-11-29 19:36:37 +0700

    #REFACTORING

--- src/com/wh1lec0d3r_/bunchsk/core/client/CoreClient.java
+++ src/com/wh1lec0d3r_/bunchsk/core/client/CoreClient.java
@@ -16,36 +16,44 @@ public class CoreClient extends Thread {
     private Socket socket;
 
     public CoreClient(ConfigData configData) {
-        System.out.println("Loading...");
+
+        System.out.println("█ Client » Loading");
+
         this.configData = configData;
-        this.openSocket(configData.host, configData.port);
-        this.sendData();
-        this.readData();
+        System.out.println("█ Client » Connecting to server socket");
+        this.openSocket(configData.getHost(), configData.getPort());
+
+        System.out.println("█ Client » Authorizing");
+        this.authorize();
     }
 
-    private void sendData() {
+    private void authorize() {
+        DataInputStream dataInputStream;
+        DataOutputStream dataOutputStream;
         try {
-            DataOutputStream dataOutputStream = new DataOutputStream(this.getSocket().getOutputStream());
+
+            dataInputStream = new DataInputStream(this.getSocket().getInputStream());
+            dataOutputStream = new DataOutputStream(this.getSocket().getOutputStream());
+
             dataOutputStream.writeUTF(Bukkit.getWorldContainer().getCanonicalFile().getName());
-            dataOutputStream.writeUTF(this.getConfigData().password);
-            dataOutputStream.writeInt(this.getConfigData().hashId);
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
+            dataOutputStream.writeUTF(this.getConfigData().getPassword());
+            dataOutputStream.writeInt(this.getConfigData().getHashId());
 
-    private void readData() {
-        try {
-            DataInputStream dataInputStream = new DataInputStream(this.getSocket().getInputStream());
-            if(dataInputStream.readBoolean()) {
+            boolean auth = dataInputStream.readBoolean();
+
+            if(auth) {
+                System.out.println("█ Client » Authorized");
+                System.out.println("█ Client » Starting » Thread » Reading data");
                 this.start();
-                System.out.println("logged in");
+
             } else {
-                System.out.println("i am closed :c");
-                this.getSocket().close();
+                System.out.println("█ Client » Error on authorize");
             }
 
+
         } catch (IOException ex) {
+
+            System.out.println("█ Client Error on authorize: " + ex.getMessage());
             ex.printStackTrace();
         }
     }
@@ -53,33 +61,41 @@ public class CoreClient extends Thread {
     private void openSocket(String host, int port) {
 
         try {
+
             this.socket = new Socket(host, port);
-            System.out.println("socket opened");
+            System.out.println("█ Client » Connected to socket");
         } catch (IOException e) {
-            System.out.println("error on opening socket");
-            e.printStackTrace();
+
+            System.out.println("█ Client » Error on connecting to socket: " + e.getMessage());
         }
     }
 
     public void run() {
+
         while (!this.getSocket().isClosed()) {
+
             try {
+
                 DataInputStream dataInputStream = new DataInputStream(this.getSocket().getInputStream());
                 short id = dataInputStream.readShort();
 
                 YPacket yPacket = PacketManager.getPacket(id);
+
                 if(yPacket != null) {
-                    //yPacket.setSocket(this.getSocket());
-                    //yPacket.setClientHandler(this);
+
                     yPacket.read(dataInputStream);
                     yPacket.handle();
                 }
                 Thread.sleep(10L);
+
             } catch (IOException | InterruptedException ex) {
+
                 try {
+
                     this.getSocket().close();
                 } catch (IOException e) {
-                    e.printStackTrace();
+
+                    System.out.println("█ Client » Error on reading data: " + e.getMessage());
                 }
 
                 this.stop();
